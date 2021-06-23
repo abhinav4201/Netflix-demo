9afb76b5725e0bf4d9c58b9772446de77efc9d50
abhi4201 <abhinav445.aa@gmail.com>
2021-06-23 18:20:03 +0530

    changed heading
    trial working changed header value

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>this is trial</h1>
+        <h1>this is trial 2</h1>
       </header>
     </div>
   );
